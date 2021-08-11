f8bd0239170f79092aeb9eb5057616775ee76688
Build Xuan Ngoc <ngocbui.se@gmail.com>
2021-08-11 13:28:55 +0700

    change output image to webp extension

--- src/app.ts
+++ src/app.ts
@@ -19,25 +19,26 @@ app.get('/', async(req: Request, res: Response) => {
       .get(fileURL, { responseType: 'arraybuffer' })
       .then(res => Buffer.from(res.data, 'binary'));
 
-      let thumbnail: Buffer = await sharp(imageBuffer).resize(100, 67).toBuffer();
+    let thumbnail: Buffer = await sharp(imageBuffer).resize(100, 67).toBuffer();
 
-      let image: Sharp = sharp(imageBuffer)
-        .resize(300, 200)
-        .composite([{
-          input: thumbnail,
-          left: 0,
-          top: 200 - 67
-        }])
+    let image: Sharp = sharp(imageBuffer)
+      .resize(300, 200)
+      .composite([{
+        input: thumbnail,
+        left: 0,
+        top: 200 - 67
+      }])
+      .webp({quality: 100})
 
-      let resultImageBuffer: Promise<Buffer> = image.toBuffer();
+    let resultImageBuffer: Promise<Buffer> = image.toBuffer();
 
-      await Promise.all([image.toFile('out.webp'), resultImageBuffer]);
+    await Promise.all([image.toFile('out.webp'), resultImageBuffer]);
 
-      resultImageBuffer.then((buffer: Buffer) => {
-        res.writeHead(200, {'Content-Type': 'image/webp'});
+    resultImageBuffer.then((buffer: Buffer) => {
+      res.writeHead(200, {'Content-Type': 'image/webp'});
 
-        res.end(buffer, 'binary');
-      })
+      res.end(buffer, 'binary');
+    })
 
   } catch (error) {
     res.status(422).send(error.message).end();
